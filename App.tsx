41a1a50d7370893b6be9673a9273785d0a9c3f88
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-10-22 10:37:25 +0200

    ft: add lib and create navigation stack

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { CategoriesScreen } from './src/screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MilsCategory" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
